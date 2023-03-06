c2271a484e7e7a4a89c6cf614c675a029f8bae41
maxim <glin.maxim@yandex.ru>
2023-03-06 20:54:21 +0300

    lesson 7 day 5

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
 
     def __str__(self):
         return self.question_text
