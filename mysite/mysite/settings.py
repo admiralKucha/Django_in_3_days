9657cbd32adc229441100f6b95fe84dcb5436dc7
maxim <glin.maxim@yandex.ru>
2023-03-08 13:42:39 +0300

    lesson 7.7.1 day 7

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tibde+36654tc(dr%c99*y8)%rs@h&cra-@1e&*7+wn6ygh3@-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", '.vercel.app']
 
