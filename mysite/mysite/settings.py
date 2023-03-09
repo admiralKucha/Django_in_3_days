cbf46a8e97f34422cf802f240e8aad1cb6dd9a5c
maxim <glin.maxim@yandex.ru>
2023-03-09 19:02:38 +0300

    lesson 7.7.8 day 8

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 STATICFILES_DIRS = [BASE_DIR/'static']
 STATIC_ROOT = BASE_DIR/'staticfiles'
 
