26fd05459a2e5a55fc9518ff9a84d9eb5c733523
maxim <glin.maxim@yandex.ru>
2023-03-09 19:51:56 +0300

    lesson 7.7.12 day 8

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
